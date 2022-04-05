d9b08b8a7befad149b1fe571d7a8ecce6a5ca0d3
Rob Flickenger <rob@hackerfriendly.com>
2022-04-04 21:25:33 -0700

    Download the model

--- install.sh
+++ install.sh
@@ -7,5 +7,9 @@ if [ -z "$VIRTUAL_ENV" ]; then
   exit 1
 fi
 
+# Download the model
+mkdir -p models/ldm/text2img-large/
+wget -c -O models/ldm/text2img-large/model.ckpt https://ommer-lab.com/files/latent-diffusion/nitro/txt2img-f8-large/model.ckpt
+
 pip install torch==1.7.0+cu110 'torchvision==0.8.1+cu110' torchaudio==0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
 pip install -r requirements.txt
